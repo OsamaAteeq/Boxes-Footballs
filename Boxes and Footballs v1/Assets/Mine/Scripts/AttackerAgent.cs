7260c76aa10e65b8c5da249e1a76eedf35e32566
OsamaAteeq <85699794+OsamaAteeq@users.noreply.github.com>
2021-12-25 00:50:22 +0500

    gitignore include venv +small game changes

---
+++ Boxes and Footballs v1/Assets/Mine/Scripts/AttackerAgent.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.MLAgents;
+using Unity.MLAgents.Sensors;
+using Unity.MLAgents.Actuators;
+
+public class AttackerAgent : Agent
+{
+    public override void OnActionReceived(ActionBuffers actions)
+    {
+    }
+}
